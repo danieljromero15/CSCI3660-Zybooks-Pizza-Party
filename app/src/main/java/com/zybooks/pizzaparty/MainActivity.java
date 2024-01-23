5e5d38b3166122c0854012962612fa7e910a55b2
danieljromero15 <danieljromero305@gmail.com>
2024-01-23 11:56:42 -0500

    Completed Debugging

--- app/src/main/java/com/zybooks/pizzaparty/MainActivity.java
+++ app/src/main/java/com/zybooks/pizzaparty/MainActivity.java
@@ -5,9 +5,14 @@ import android.view.View;
 import android.widget.EditText;
 import android.widget.RadioGroup;
 import android.widget.TextView;
+
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.util.Log;
+
 public class MainActivity extends AppCompatActivity {
+
+    private final static String TAG = "MainActivity";
 
     public final int SLICES_PER_PIZZA = 8;
 
@@ -19,6 +24,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Log.d(TAG, "onCreate was called");
+
         mNumAttendEditText = findViewById(R.id.num_attend_edit_text);
         mNumPizzasTextView = findViewById(R.id.num_pizzas_text_view);
         mHowHungryRadioGroup = findViewById(R.id.hungry_radio_group);
@@ -26,26 +34,31 @@ public class MainActivity extends AppCompatActivity {
 
     public void calculateClick(View view) {
         String numAttendStr = mNumAttendEditText.getText().toString();
-        int numAttend = Integer.parseInt(numAttendStr);
+        Log.d(TAG, "Number is " + numAttendStr);
+        int numAttend;
+        try {
+            numAttend = Integer.parseInt(numAttendStr);
+        } catch (NumberFormatException e){
+            numAttend = 0;
+        }
 
         // Determine how many slices on average each person will eat
-        int slicesPerPerson = 0;
+        int slicesPerPerson;
         int checkedId = mHowHungryRadioGroup.getCheckedRadioButtonId();
         if (checkedId == R.id.light_radio_button) {
             slicesPerPerson = 2;
-        }
-        else if (checkedId == R.id.medium_radio_button) {
+        } else if (checkedId == R.id.medium_radio_button) {
             slicesPerPerson = 3;
-        }
-        else if (checkedId == R.id.ravenous_radio_button) {
+        } else if (checkedId == R.id.ravenous_radio_button) {
             slicesPerPerson = 4;
-        }else{
-            mNumPizzasTextView.setText("Please let us know how hungry they are!");
+        } else {
+            mNumPizzasTextView.setText(getString(R.string.no_selection_error)); // Please let us know how hungry they are!
             return;
         }
 
         int totalPizzas = (int) Math.ceil(numAttend * slicesPerPerson /
                 (double) SLICES_PER_PIZZA);
-        mNumPizzasTextView.setText("Total pizzas: " + totalPizzas);
+        Log.d(TAG, getString(R.string.total, totalPizzas));
+        mNumPizzasTextView.setText(getString(R.string.total, totalPizzas));
     }
 }
